a3805b22d22d67cf871794c2db612fd082cd54cb
BasS12009 <149912863+BasS12009@users.noreply.github.com>
2024-05-31 07:13:25 -0700

    Add files via upload

---
+++ CUVehiculo_DTO/src/main/java/com/mycompany/cuvehiculo_dto/CUVehiculo_DTO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cuvehiculo_dto;
+
+/**
+ *
+ * @author diana
+ */
+public class CUVehiculo_DTO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
